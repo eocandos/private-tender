274299244da148855f1f48f2547b043dd271df0a
Erick <eaocandos@gmail.com>
2020-02-23 10:31:57 -0500

    Empty project: Java11, jpa, security

---
+++ private.tenders-be/src/main/java/com/example/demo/PrivateTendersApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateTendersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateTendersApplication.class, args);
+	}
+
+}
